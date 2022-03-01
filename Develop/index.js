5bd2dd4b8d79421bf56201886af9cca337aaeb6a
CPalileo <cpalileo@gmail.com>
2022-02-28 21:21:39 -0800

    first commit?

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,72 @@
-// TODO: Include packages needed for this application
+//----Required Packages----//
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const fs = require("fs");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//----Questions array to assemble readme content----//
 
-// TODO: Create a function to initialize app
-function init() {}
+const content = [
+  {
+    type: "input",
+    message: "What is the title of your?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "How would you describe your project?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What are the required installations?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Provide instructions for usage.",
+    name: "usage",
+  },
+  {
+    type: "list",
+    message: "Choose your license:",
+    choices: ["MIT License", "GNU GPLv3", "Unlicense"],
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "List the contributing members",
+    name: "contributors",
+  },
+  {
+    type: "input",
+    message: "Provide testing information.",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: "Provide you Github account information.",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is your contact email address?",
+    name: "email",
+  },
+];
 
-// Function call to initialize app
+//----App Functions----//
+
+function init() {
+  inquirer
+    .prompt(content)
+    .then((data) => writeToFile("README.md", generateMarkdown(data)))
+    .then(() => console.log("README.md created"));
+}
+
+function writeToFile(fileName, data, options) {
+  return fs.writeFileSync(fileName, data, options);
+}
+
+//----Initialize app----//
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,74 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// ----License badge---- //
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "GNU GPL 3.0":
+      return "https://opensource.org/licenses/GPL-3.0";
+    default:
+      return "";
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// ----License link---- //
+
 function renderLicenseLink(license) {}
+switch (license) {
+  case "MIT":
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)]";
+  case "GNU GPL 3.0":
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+  case "Unlicense":
+    return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]";
+  default:
+    return "";
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// ----Return license info to readme---- //
 
-// TODO: Create a function to generate markdown for README
+function renderLicenseSection(license) {
+  return license !== "none" ? `${license}\n${renderLicenseLink(license)}` : "";
+}
+
+// ----Generate markdown for README---- //
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Table of Contents
+  * [Description](#Description)
+  * [Installation](#Installation-Instructions)
+  * [Usage Instructions](#Usage-Instructions)
+  * [License](#License)
+  * [Contributing Members](#Contributing-Members)
+  * [Testing](#Testing)    
+  * [Questions/Inquiries](#Questions/Inquiries)
+
+  ## Description
+  ${data.description}
+
+
+  ## Installation Instructions 
+  ${data.installation}
+
+  ## Usage Instructions
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributing Members
+  ${data.contributors}
+
+  ## Testing 
+   ${data.test}
+
+  ## Questions/Inquiries 
+
+  ### GitHub Profile
+  [GitHub Profile](http://github.com/${data.github})
+
+  ### Email
+  ${data.email}
 `;
 }
-
 module.exports = generateMarkdown;
